e59e9c8e5b9a8c348e343150c1297282ec61ef25
Ray Samuels <ray@samuels.io>
2020-05-01 22:59:19 +0100

    Updates

---
+++ precache-manifest.fc5cbbe5cc8113f7212f8d445dd3e0f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af0c19e879ce44e7c43663b3d220368",
+    "url": "/corvidapp/index.html"
+  },
+  {
+    "revision": "bf535afddd867044e2d3",
+    "url": "/corvidapp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "82c444f9d7dc3a931bf0",
+    "url": "/corvidapp/static/js/2.c74c96b0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corvidapp/static/js/2.c74c96b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf535afddd867044e2d3",
+    "url": "/corvidapp/static/js/main.720e8d13.chunk.js"
+  },
+  {
+    "revision": "e9b603ed96ff703e5da0",
+    "url": "/corvidapp/static/js/runtime-main.96d1f21f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corvidapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
